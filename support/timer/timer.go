b005521afa04e08d65ab8e9b1089837198184bf5
Flc゛ <four_leaf_clover@foxmail.com>
2023-04-20 15:04:45 +0800

    feat: Added `timer.Tick` and `timer.After` (#13)

---
+++ support/timer/timer.go
@@ -0,0 +1,25 @@
+// build:
+package timer
+
+import (
+	"time"
+)
+
+func Tick(duration time.Duration, callback func(ticker *time.Ticker)) *time.Ticker {
+	ticker := time.NewTicker(duration)
+
+	go func() {
+		for {
+			<-ticker.C
+
+			callback(ticker)
+		}
+	}()
+
+	return ticker
+}
+
+// After Nonblocking call to callback after duration.
+func After(duration time.Duration, callback func()) *time.Timer {
+	return time.AfterFunc(duration, callback)
+}
